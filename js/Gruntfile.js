43a523809dc9a004bf2ea5ecacf847b040c7fa51
deepak <deepz.m@gmail.com>
2014-04-28 00:02:53 +0530

    added basic angular files, made js build simpler

--- js/Gruntfile.js
+++ js/Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
  
         karma: {
             unit: {
-                configFile: 'karma.conf.js'
+                configFile: 'test/karma.conf.js'
             }
         }
     });
---
+++ js/test/karma.conf.js
@@ -0,0 +1,55 @@
+module.exports = function(config) {
+    config.set({
+ 
+        // base path, that will be used to resolve files and exclude
+        basePath: '../',
+ 
+        // frameworks to use
+        frameworks: ['jasmine'],
+
+        // for coverage
+        // preprocessors : {
+        //   'src/script/*.js': 'coverage'
+        // },
+ 
+        // list of files / patterns to load in the browser
+        files: [
+            'src/script/*.js',
+            'test/unit/*.js'
+        ],
+ 
+        // list of files to exclude
+        exclude: [
+            'src/script/gol.js',
+        ],
+ 
+        // test results reporter to use
+        reporters: ['progress'
+        // , 'coverage'
+        ],
+
+        plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-phantomjs-launcher',
+            'karma-jasmine',
+            'karma-coverage'
+            ],
+ 
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+ 
+        // Start these browsers
+        browsers: ['PhantomJS'],
+ 
+        // coverageReporter : {
+        //   type : 'html',
+        //   dir : 'coverage/'
+        // },
+
+        junitReporter : {
+          outputFile: 'test_out/unit.xml',
+          suite: 'unit'
+        }
+    });
+};
